12093f4df615774e60791974a78ccbad5d1b2fd7
APikielny <apikielny@gmail.com>
2020-11-03 07:54:29 -0800

    working for single channel

--- lab5.py
+++ lab5.py
@@ -31,11 +31,6 @@ def blended_image(im1, im2, alpha_matte):
         np.multiply((1 - alpha_matte),im2))
     return blended
 
-apple = cv2.imread('burt_apple.png')
-orange = cv2.imread('burt_orange.png')
-a = alpha_matte(apple.shape, 100, apple.shape[1]//2)
-b = blended_image(apple, orange, a)
-
 # cv2.imshow('Blended Output', b)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
@@ -44,12 +39,21 @@ b = blended_image(apple, orange, a)
 def laplacian_pyramid(a, level, inList):
     if (a.shape[0] < 32) or (a.shape[1] < 32) or (level == 0):
         inList.append(a)
+        # cv2.imshow("bottom", a)
+        # cv2.waitKey(0)
         return inList
         # return np.array(a)
     else:
         # Appy Gaussian filter and downsample images a and b, by factor of 2
-        blurred_a = ndimage.gaussian_filter(a, sigma=5)
+        # blurred_a = np.stack((ndimage.gaussian_filter(a[:,:,0], sigma=5), ndimage.gaussian_filter(a[:,:,1], sigma=5), ndimage.gaussian_filter(a[:,:,2], sigma=5)), axis = 2)
+        blurred_a = ndimage.gaussian_filter(a, sigma=10)
+        cv2.imshow("a " + str(level), a)
+        cv2.waitKey(0)
+        cv2.imshow("blurred " + str(level), blurred_a)
+        cv2.waitKey(0)
         details = a - blurred_a
+        cv2.imshow("details " + str(level), details)
+        cv2.waitKey(0)
         inList.append(details)
         return laplacian_pyramid(resize(blurred_a, (blurred_a.shape[0] // 2, blurred_a.shape[1] // 2), anti_aliasing=False), level-1, inList)
         # return np.array((details, laplacian_pyramid(resize(blurred_a, (blurred_a.shape[0] // 2, blurred_a.shape[1] // 2), anti_aliasing=False), level-1)))
@@ -61,41 +65,44 @@ def gaussian_pyramid(a, level, inList):
         return inList
     else:
         # Appy Gaussian filter and downsample images a and b, by factor of 2
-        blurred_a = ndimage.gaussian_filter(a, sigma=5)
+        blurred_a = ndimage.gaussian_filter(a, sigma=10)
+        # cv2.imshow("blurred " + str(level), blurred_a)
+        # cv2.waitKey(0)
         inList.append(blurred_a)
         return gaussian_pyramid(resize(blurred_a, (blurred_a.shape[0] // 2, blurred_a.shape[1] // 2), anti_aliasing=False), level-1, inList)
 
 def blend(img1, img2, mask):
-    level = 3
+    level = 2
     L1 = laplacian_pyramid(img1, level, [])
     L2 = laplacian_pyramid(img2, level, [])
     Gm = gaussian_pyramid(mask, level, [])
-    cv2.imshow("L1.1", L1[0] /255.0)
-    cv2.waitKey(0)
-    cv2.imshow("Gm.1", Gm[0] /255.0)
-    cv2.waitKey(0)
+    # for img in L1:
+    #     cv2.imshow("L", img)
+    #     cv2.waitKey(0)
+    # cv2.imshow("L1.1", L1[0] /255.0)
+    # cv2.waitKey(0)
+    # cv2.imshow("Gm.1", Gm[0] /255.0)
+    # cv2.waitKey(0)
 
 
     L0 = []
     L0resized = []
+    # for l in range(1, level + 1):
     for l in range(level + 1):
-        GmStacked = np.stack((Gm[l],Gm[l],Gm[l]), axis = -1)
-        L0.append(GmStacked[l] * L1[l] + (1 - GmStacked[l] * L2[l]))
+
+        # GmStacked = np.stack((Gm[l],Gm[l],Gm[l]), axis = -1)
+        GmStacked = Gm
+        L0.append(GmStacked[l] * L1[l] + ((1 - GmStacked[l]) * L2[l]))
+    for img in L0:
+        cv2.imshow("L0 img layer", img)
+        cv2.waitKey(0)
     for l in range(level + 1):
+    # for l in range(level):
         L0resized.append(resize(L0[l], img1.shape, anti_aliasing = False))
     npL0 = np.array(L0resized)
-    img = np.sum(npL0, axis = 0)
+    img = np.sum(npL0, axis = 0) #TODO supposed to sum here?
 
     return img
-    
-
-apple = cv2.imread('burt_apple.png')
-orange = cv2.imread('burt_orange.png')
-mask = alpha_matte(apple.shape, 100, apple.shape[1]//2)
-
-blendedImg = blend(apple, orange, mask)
-cv2.imshow("blended", blendedImg / 255.0)
-cv2.waitKey(0)
 
 def imalign_pyramid(a,b,level):
     '''
@@ -124,3 +131,18 @@ def imalign_pyramid(a,b,level):
         shift_vector = shift_vector + imalign(a, b, 2)
 
     return shift_vector
+
+# apple = cv2.imread('burt_apple.png')
+# orange = cv2.imread('burt_orange.png')
+# a = alpha_matte(apple.shape, 100, apple.shape[1]//2)
+# b = blended_image(apple, orange, a)
+
+apple = cv2.imread('burt_apple.png')/255.0
+orange = cv2.imread('burt_orange.png')/255.0
+mask = alpha_matte(apple.shape, 100, apple.shape[1]//2)
+
+# blendedImg = blend(ndimage.gaussian_filter(apple[:,:,1], sigma=1), ndimage.gaussian_filter(orange[:,:,1], sigma=1), mask)
+blendedImg = blend(apple[:,:,1], orange[:,:,1], mask)
+
+cv2.imshow("blended", blendedImg)
+cv2.waitKey(0)
